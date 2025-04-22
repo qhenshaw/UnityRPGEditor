d90e4d076e8ca5feab080fa6417e7cc4650d8da1
Quin Henshaw <qhenshaw@gmail.com>
2025-04-22 14:56:54 -0700

    added basic functionality for character, class, weapon, and skill data

---
+++ Runtime/ItemData.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+namespace UnityRPGEditor
+{
+    public abstract class ItemData : ScriptableObject
+    {
+        [field: SerializeField]
+        public string Name { get; private set; }
+
+        [field: SerializeField]
+        public string Description { get; private set; }
+
+        [field: SerializeField, PreviewField(Height = 50)]
+        public Sprite Icon { get; private set; }
+
+        [field: SerializeField, SuffixLabel("Gold", true)]
+        public int Value { get; private set; }
+
+        [field: SerializeField, SuffixLabel("kg", true)]
+        public float Weight { get; private set; }
+
+        [field: SerializeField]
+        public ItemRarity Rarity { get; private set; } = ItemRarity.Common;
+    }
+
+    public enum ItemRarity
+    {
+        Junk,
+        Common,
+        Uncommon,
+        Magic,
+        Rare,
+        Epic,
+        Legendary,
+        Mythic
+    }
+}
